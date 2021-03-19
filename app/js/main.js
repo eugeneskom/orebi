6fe778c05fc57c3f0e055921ea59e07f7427476a
eugeneskom <eugeneskom@gmail.com>
2021-03-19 18:57:27 +0200

    worked on dropw down menu

--- app/js/main.js
+++ app/js/main.js
@@ -17,22 +17,27 @@ $(function () {
     arrows: true,
   });
   $('.user').on('click', function () {
-    $('.user__list').toggleClass('user__list--active')
+    $('.user__list').toggleClass('active')
   });
   $('.cart').on("click", function () {
-    $('.cart__inner').toggleClass('cart__inner--active');
+    $('.cart__inner').toggleClass('active');
   });
 
-  $('.filter').on("click", function () {
-    $('.filter__list').toggleClass('filter__list--active');
+  $('.dropdown-menu').on("click", function () {
+    $('.dropdown-menu').toggleClass('dropdown-menu--active');
+    $('.dropdown-menu__list').toggleClass('active');
+
   });
 
   $(document).on("click", function (event) {
     //if you click on anything except the modal itself or the "open modal" link, close the modal
-    if (!$(event.target).closest(".filter").length) {
-      $("body").find(".filter__list").removeClass("filter__list--active");
+    if (!$(event.target).closest(".dropdown-menu").length) {
+      $("body").find(".dropdown-menu").removeClass("dropdown-menu--active");
+      $("body").find(".dropdown-menu__list").removeClass("active");
     }
   });
+ 
+
 
   $('.shop-content__btn').on('click', function(){
     $('.shop-content__btn').removeClass('shop-content__btn--active');
@@ -46,4 +51,15 @@ $(function () {
     $('.product').removeClass('product--list');
   });
 
+  const menuBtn = document.querySelector('.menu-btn');
+  let menuOpen = false;
+  menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+      menuBtn.classList.add('open');
+      menuOpen = true;
+    } else{
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+    }
+  });
 });
